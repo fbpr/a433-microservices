a59d43fca8af9d744667e8b7d417977e8fd5c030
fbpr <71777049+fbpr@users.noreply.github.com>
2023-06-07 02:45:43 +0700

    add shell script for build and push to github packages

---
+++ build_push_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "build the docker image" # menampilkan teks
+docker build -t item-app:v1 . # membuat image dari Dockerfile pada current directory dan memberinya label item-app serta tag v1
+sleep 2 #jeda 2 detik
+
+echo "list images" # menampilkan teks
+docker images # menampilkan daftar local image
+sleep 2 #jeda 2 detik
+
+echo "rename the image" # menampilkan teks
+docker tag item-app:v1 ghcr.io/fbpr/a433-microservices:v1 # mengubah nama image
+sleep 2 #jeda 2 detik
+
+echo "login GitHub Packages" # menampilkan teks
+echo "$CR_PAT" | docker login ghcr.io -u "fbpr" --password-stdin # melakukan login ke github packages dengan token
+sleep 2 #jeda 2 detik
+
+echo "push the image" # mengubah nama image 
+docker push ghcr.io/fbpr/a433-microservices:v1 # mengunggah hasil build image ke github packages 
